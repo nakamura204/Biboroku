ee90cede4dd7d2e49d0669f90c05cd0984c7deb7
nakamura204 <s1270204@u-aizu.ac.jp>
2022-11-29 03:04:50 +0900

    second com mit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,13 +6,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://nakamura204.github.io/',
+  baseUrl: '/Biboroku/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'nakamura204', // Usually your GitHub org/user name.
+  projectName: 'Biboroku', // Usually your repo name.
 
   presets: [
     [
