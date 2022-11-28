290b75fa3bef704f1761931de9e3811da83e9c60
yukimaru5310 <k.teji0621@gmail.com>
2022-11-29 06:16:34 +0900

    [fix] introduction.md

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,7 +49,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'introduction',
             position: 'left',
             label: 'Tutorial',
           },
@@ -69,7 +69,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/introduction',
               },
             ],
           },
